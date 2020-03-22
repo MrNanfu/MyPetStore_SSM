301c01145785155fdd836a5cfd03f9cbc041b126
MrNanfu <chenyanting0705>
2020-03-22 16:48:58 +0800

    init MypetStore Project

---
+++ src/main/java/org/csu/mypetstore_smm/MypetstoreSmmApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstore_smm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreSmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetstoreSmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/csu/mypetstore_smm/MypetstoreSmmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstore_smm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoreSmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
